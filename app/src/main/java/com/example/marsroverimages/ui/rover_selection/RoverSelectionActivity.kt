797dab2c2d3ffab639b09473f4720f43f0491b69
Tanvir Ahmod <tanvirahmodcse@gmail.com>
2020-08-08 19:33:21 +0600

    Setup view pager and tablayout

---
+++ app/src/main/java/com/example/marsroverimages/ui/rover_selection/RoverSelectionActivity.kt
@@ -0,0 +1,23 @@
+package com.example.marsroverimages.ui.rover_selection
+
+import android.os.Bundle
+import com.example.marsroverimages.R
+import com.example.marsroverimages.base.ui.BaseActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_rover_selection.*
+
+@AndroidEntryPoint
+class RoverSelectionActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rover_selection)
+
+        val roverSelectionAdapter = RoverSelectionAdapter(this, 3)
+        pager.adapter = roverSelectionAdapter
+
+        TabLayoutMediator(tab_layout, pager) { _, _ ->
+        }.attach()
+
+    }
+}
--- app/src/main/java/com/example/marsroverimages/ui/rover_selection/RoverSelectionFragment.kt
+++ app/src/main/java/com/example/marsroverimages/ui/rover_selection/RoverSelectionFragment.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.marsroverimages.R
 import com.example.marsroverimages.base.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RoverSelectionFragment : BaseFragment() {
+
+    companion object {
+        const val ARG_POSITION = "position"
+
+        fun getInstance(position: Int): Fragment {
+            val roverSelectionFragment = RoverSelectionFragment()
+            val bundle = Bundle()
+            bundle.putInt(ARG_POSITION, position)
+            roverSelectionFragment.arguments = bundle
+            return roverSelectionFragment
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
