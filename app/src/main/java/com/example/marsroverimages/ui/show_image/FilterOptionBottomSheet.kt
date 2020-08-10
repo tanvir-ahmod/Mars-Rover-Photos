d0e0a7384555abd62627f44ce33a358c3794f6fa
Tanvir Ahmod <tanvirahmodcse@gmail.com>
2020-08-10 15:53:36 +0600

    Add filter search page

---
+++ app/src/main/java/com/example/marsroverimages/ui/show_image/FilterOptionBottomSheet.kt
@@ -0,0 +1,48 @@
+package com.example.marsroverimages.ui.show_image
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.marsroverimages.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.dialog_filter.view.*
+import java.util.*
+
+class FilterOptionBottomSheet : BottomSheetDialogFragment() {
+    val selectedDate = Calendar.getInstance()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_filter, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.iv_date_picker.setOnClickListener {
+
+            val mDatePicker: DatePickerDialog
+            mDatePicker = DatePickerDialog(
+                requireContext(),
+                DatePickerDialog.OnDateSetListener { _, selectedYear, selectedMonth, selectedDay ->
+
+                    selectedDate.set(Calendar.YEAR, selectedYear)
+                    selectedDate.set(Calendar.MONTH, selectedMonth)
+                    selectedDate.set(Calendar.DAY_OF_MONTH, selectedDay)
+
+                    val selectedDateText = "$selectedDay-$selectedMonth-$selectedYear"
+                    view.tv_date.text = selectedDateText
+                },
+                selectedDate.get(Calendar.YEAR),
+                selectedDate.get(Calendar.MONTH),
+                selectedDate.get(Calendar.DAY_OF_MONTH)
+            )
+            mDatePicker.datePicker.maxDate = Date().time
+            mDatePicker.show()
+        }
+    }
+}
--- app/src/main/java/com/example/marsroverimages/ui/show_image/ShowImageActivity.kt
+++ app/src/main/java/com/example/marsroverimages/ui/show_image/ShowImageActivity.kt
@@ -1,10 +1,12 @@
 package com.example.marsroverimages.ui.show_image
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.marsroverimages.R
 import com.example.marsroverimages.base.ui.BaseActivity
-import com.example.marsroverimages.models.RoverData
 import com.example.marsroverimages.models.RoverPhoto
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_show_image.*
@@ -21,5 +23,25 @@ class ShowImageActivity : BaseActivity() {
         val roverImageAdapter = RoverImageAdapter(dummyRoverData)
         rv_images.layoutManager = GridLayoutManager(this, 2)
         rv_images.adapter = roverImageAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.filter_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_filter -> {
+                val filterOptionDialogFragment = FilterOptionBottomSheet()
+                filterOptionDialogFragment.show(
+                    supportFragmentManager,
+                    filterOptionDialogFragment.tag
+                )
+                true
+            }
+            else -> false
+        }
     }
 }
