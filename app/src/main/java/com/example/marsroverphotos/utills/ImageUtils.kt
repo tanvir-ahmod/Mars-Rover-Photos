727b9df8c33dfd3eb148d781afd313abb276e3a9
tanvirahmod <tanvirahmodcse@gmail.com>
2021-01-14 20:03:24 +0600

    Replace image details UI with jetpack compose

---
+++ app/src/main/java/com/example/marsroverphotos/utills/ImageUtils.kt
@@ -0,0 +1,32 @@
+package com.example.marsroverphotos.utills
+
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.AmbientContext
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+
+@Composable
+fun loadPicture(url: String): MutableState<Bitmap?> {
+
+    val bitmapState: MutableState<Bitmap?> = mutableStateOf(null)
+    // get network image
+    Glide.with(AmbientContext.current)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) { }
+                override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                ) {
+                    bitmapState.value = resource
+                }
+            })
+
+    return bitmapState
+}
